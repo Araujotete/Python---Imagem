adac0ba72e0a7670e42583b51634c6317af928b8
Ester Araújo <esteraraujo0805@gmail.com>
2025-03-07 19:48:14 -0300

    Add files via upload

--- image_pillow.py
+++ image_pillow.py
@@ -8,10 +8,26 @@ imagem.show()
 # converter (save as jpg)
 # tentar direto, se der erro, tornar RGB e converter em jpg
 imagem_rgb = imagem.convert("RGB")
-imagem_rgb.save("imagens/python lira.jpg")
+imagem_rgb.save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
 
 # resize (muito util em servidores/sites) - sempre partindo da maior para a menor, se n você zoa o rolê
 tamanho = (500, 500)
 imagem.thumbnail(tamanho)
-imagem.save("imagens/python lira 500.png")
+imagem.save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
 
+tamanho = (300, 300)
+imagem.thumbnail(tamanho)
+imagem.save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
+
+# editar a imagem
+
+# rotate
+imagem.rotate(180).save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
+
+# preto e branco
+imagem.convert("L").save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
+
+# filtros
+from PIL import ImageFilter # type: ignore
+
+imagem.filter(ImageFilter.GaussianBlur(20)).save("c:\Users\Robson Araújo\Desktop\Ester Araújo\Imagens\java imagem.jpg")
